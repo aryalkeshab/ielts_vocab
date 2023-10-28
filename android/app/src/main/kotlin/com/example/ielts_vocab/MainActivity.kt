12957d87f91e5d4241d878ccbb353e044d1031c6
Keshab Aryal <kesab.nipunasewa@gmail.com>
2023-10-28 22:01:31 +0545

    sada

--- android/app/src/main/kotlin/com/example/ielts_vocab/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ielts_vocab/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.vocabgeek.sat
+package com.vocabgeek.act
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/shared/urls.dart
+++ lib/shared/urls.dart
@@ -2,13 +2,13 @@ const moreAppsurl =
     'https://play.google.com/store/apps/dev?id=5329737104439787859&fbclid=IwAR1aMwctUNViSWjpq_F06SOzvDuw3vDIV7MoeOLr93KfBbGKSSHamInfvAs';
 
 const rateAppurl =
-    "https://play.google.com/store/apps/details?id=com.vocabgeek.sat";
+    "https://play.google.com/store/apps/details?id=com.vocabgeek.act";
 
 const shareUrl =
-    "Install App from: https://play.google.com/store/apps/details?id=com.vocabgeek.sat";
+    "Install App from: https://play.google.com/store/apps/details?id=com.vocabgeek.act";
 
-const googlePlayIdentifier = "com.vocabgeek.sat";
+const googlePlayIdentifier = "com.vocabgeek.act";
 
-const appTitle = "SAT Flashcards - VocabGeek";
+const appTitle = "ACT Flashcards - VocabGeek";
 
-const api = "sat";
+const api = "act";
